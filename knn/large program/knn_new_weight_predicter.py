6e916ab8011a344b0fc93f8c5a15cc41467a7030
aathmapriyaa <aathmapriyaa@gmail.com>
2025-08-07 11:17:42 +0530

    project created

--- knn/large program/knn_new_weight_predicter.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as pt
-import sklearn.neighbors as knn
-mydata=pd.read_csv("new_weight_predicter.csv")
-x= mydata[["height"]]
-y= mydata[["weight"]] 
-model=knn.KNeighborsRegressor(n_neighbors=3)
-model.fit(x,y)
-print(model.predict[[160]])
